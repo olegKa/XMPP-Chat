697bc3667c3a229e1626eec06db025067a070c7c
kalinin <o.kalinin@compassplus.com>
2019-01-22 11:39:54 +0500

    Initial Commit

---
+++ XMPPTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMPPTest
+//
+//  Created by OLEG KALININ on 22.01.2019.
+//  Copyright © 2019 oki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMPPTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMPPTest
+//
+//  Created by OLEG KALININ on 22.01.2019.
+//  Copyright © 2019 oki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
